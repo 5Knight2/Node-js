f4098f3fa6569a82a6f5f15bad05e337d449b558
5Knight2 <pratiktarale258@gmail.com>
2023-06-03 12:55:16 +0530

    server with home,about us, node

--- first.js
+++ first.js
@@ -1,7 +1,18 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log('PT')
+   let address =req.url;
+   let str='';
+   if(address=='/home')str='Welcome home';
+   if(address=='/about')str='Welcome to About Us page'
+   if(address=='/node')str='Welcome to my Node Js project';
+
+res.write('<html><body>');
+
+res.write('<h1>'+str +'</h1>')
+res.write('<body></html>');
+
+
 });
 
 server.listen(4000);
