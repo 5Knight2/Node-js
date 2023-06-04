1b05f161a79ad96c4f2cc3610d7d5329397de9e8
5Knight2 <pratiktarale258@gmail.com>
2023-06-04 15:43:04 +0530

    Chat App first commit

--- first.js
+++ first.js
@@ -1,16 +1,49 @@
 const http=require('http');
-
+const fs=require('fs');
+const { buffer } = require('stream/consumers');
 const server=http.createServer((req,res)=>{
    let address =req.url;
-   let str='';
-   if(address=='/home')str='Welcome home';
-   if(address=='/about')str='Welcome to About Us page'
-   if(address=='/node')str='Welcome to my Node Js project';
+   let methods=req.method;
+   let str;
+   str=fs.readFileSync('./message.txt');
 
+   if(address=='/' ){
+      
+      if(methods=='POST'){
+         const bodydata=[];
+         req.on('data',(partofdata)=>{
+            bodydata.push(partofdata);
+         });
+         req.on('end',()=>{
+            let finalbody=Buffer.concat(bodydata).toString();
+            finalbody=finalbody.split('=')[1];
+            fs.writeFileSync('./message.txt',finalbody);
+            res.statusCode=302;
+           
+
+            res.write('<html><body>');
+            res.write('<h1>'+'Chat App' +'</h1>')
+            res.write(`<h5>${finalbody}</h5>`)
+            
+            res.write("<form action='/' method='post'><input type='text' name='message' ><button type=submit>Send</button></form>");
+            res.write('<body></html>');
+            res.end();
+         })
+      }else{
+         
 res.write('<html><body>');
+res.write('<h1>'+'Chat App' +'</h1>')
+res.write('<h5>'+str +'</h5>')
 
-res.write('<h1>'+str +'</h1>')
+res.write("<form action='/' method='post'><input type='text' name='message' ><button type=submit>Send</button></form>");
 res.write('<body></html>');
+res.end();
+      }
+
+    }
+  
+
+
 
 
 });
